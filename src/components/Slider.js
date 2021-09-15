eb9d32177fd3752d64fb4ebc9804e62426e216ba
Chad O'Brien <obrien.music@gmail.com>
2021-09-14 20:06:14 -0400

    add next and previous buttons

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,7 +2,37 @@ import React from 'react';
 import Card from "./Card";
 import cardData from "../cardData";
 
+
 class Slider extends React.Component {
+    
+    next = () => {
+        var cards = Array.from(document.getElementsByClassName("card"));
+        
+        if(cards[cards.length - 1].className === 'card focus') {
+            cards[cards.length - 1].className = 'card';
+            return;
+        }
+
+        for(let i = 0; i < cards.length - 1; i++) {
+            if (cards[i].className === "card focus") {
+                cards[i].className = 'card';
+                cards[i + 1].className = 'card focus';
+                return;
+            };
+        }
+
+        for(let i = 0; i < cards.length - 1; i++) {
+            if (cards[i].className === "card focus") {break};
+            cards[0].className = 'card focus';
+        }
+        
+        
+    }
+    
+    prev = () => {
+        console.log('previous');
+    }
+    
     render() {
         return(
             <section>
@@ -12,6 +42,8 @@ class Slider extends React.Component {
                         return <Card title={card.title} description={card.description} link={card.link}/>
                     })}
                 </article>
+                <button onClick={this.prev}>Previous</button>
+                <button onClick={this.next}>Next</button>
             </section>
         )
     }
