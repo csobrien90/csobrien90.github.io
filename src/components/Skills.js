4977840592a44d87705b3424a3be20909b4c85c6
Chad OBrien <obrien.music@gmail.com>
2022-07-04 18:27:25 -0400

    add work experience and skills data

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import skills from '../skills.json';
 
 class Skills extends React.Component {
 	render() {
 		return(
 			<article className='skills'>
 				<h3>Skills</h3>
+				<ul>
+					{skills.map((skill, index) => {
+						return <li key={index}>{skill}</li>
+					})}
+				</ul>
 			</article>
 		)
 	}
--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import workExperience from '../workExperience.json';
 
 class WorkExperience extends React.Component {
 	render() {
 		return(
 			<article className='workExperience'>
 				<h3>Work Experience</h3>
+				{
+					workExperience.map((job, index) => {
+						return <li key={index}>
+							<h4>{job.title}</h4>
+							{job.company && <p className='company'>{job.company}</p>}
+							<p className='workExperienceDates'>{job.time}</p>
+							<p className='workExperienceDescription'>{job.description}</p>
+						</li>
+					})
+				}
 			</article>
 		)
 	}
