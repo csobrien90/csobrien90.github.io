79d2981ad06b8645c3efa5f400a15e4fbe761f7d
Chad OBrien <obrien.music@gmail.com>
2022-07-04 16:07:53 -0400

    style top link

--- src/components/About.js
+++ src/components/About.js
@@ -19,8 +19,7 @@ class About extends React.Component {
 				<p>Chad O'Brien is a full stack developer as well as a freelance musician based in Louisville, Kentucky. Among his many passions are web development, 
 					playing, teaching, and writing music, urban farming, and social justice activism. He is currently a web developer for Kentucky Educational Television, 
 					Operations Manager for the Louisville Jazz Initiative, and a mentor with Code Louisville. You can get to know Chad better by checking out the projects 
-					above or 
-					<a href="mailto:obrien.music@gmail.com" target="_blank">emailing him</a>.
+					above or <a href="mailto:obrien.music@gmail.com" target="_blank">emailing him</a>.
 				</p>
 			</section>
 		)
