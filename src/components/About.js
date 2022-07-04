10e9876cc4132f5f52460273fe7bc1b499b4e1e9
Chad OBrien <obrien.music@gmail.com>
2022-07-04 15:48:59 -0400

    add border corners to profile picture

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,18 @@ import headshot from '../media/headshot.jpg';
 
 class About extends React.Component {
 	render() {
+
+		const divArr = [];
+		for (let i = 0; i < 25; i++ ) divArr.push(i);
+
 		return(
 			<section className="about" id="about">
-				<img src={headshot} alt="a headshot of Chad O'Brien dressed in a suit"></img>
+				<div className='imgWrapper'>
+					<img src={headshot} alt="a headshot of Chad O'Brien dressed in a suit"></img>
+					<div className='imgBorderWrapper'>
+						{divArr.map(boxNum => <div className='imgBorderBox'></div>)}
+					</div>
+				</div>
 				<h1>Chad O'Brien</h1>
 				<p>Chad O'Brien is a full stack developer as well as a freelance musician based in Louisville, Kentucky. Among his many passions are web development, 
 					playing, teaching, and writing music, urban farming, and social justice activism. He is currently a web developer for Kentucky Educational Television, 
