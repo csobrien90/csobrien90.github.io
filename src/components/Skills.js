ddd8bcd328e36b8cbcd07b3566c3879647c4a7ff
Chad OBrien <obrien.music@gmail.com>
2022-07-07 00:11:26 -0400

    style skills and flesh out skills data structure

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,7 +8,7 @@ class Skills extends React.Component {
 				<h3>Skills</h3>
 				<ul>
 					{skills.map((skill, index) => {
-						return <li key={index}><p>{skill}</p></li>
+						return <li key={index}><img src={skill.imageSrc} alt={skill.imageAlt}></img><p>{skill.text}</p></li>
 					})}
 				</ul>
 			</article>
